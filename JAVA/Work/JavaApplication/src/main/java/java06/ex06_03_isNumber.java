46097365590aa445df58887d8ff29b1bffb4af63
kshwang <kshwang@hanmail.net>
2017-09-28 08:58:11 +0900

    09/27

--- JAVA/Work/JavaApplication/src/main/java/java06/ex06_03_isNumber.java
+++ JAVA/Work/JavaApplication/src/main/java/java06/ex06_03_isNumber.java
@@ -9,19 +9,36 @@ public class ex06_03_isNumber {
         String str; 
 
         System.out.print("문자열을 입력 하세요: ");
-        str = keyin.next();
+        str = keyin.nextLine();
+//        str = keyin.next(); //next()는 엔터키를 인식못함..
+        
         
         boolean rslt = isNumber(str);
         System.out.println("리턴 결과: "+rslt);
     }
     
     public static boolean isNumber(String str) {
-        if (str != null && str.equals("")) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        int cnt=0;
         
+        for (int i=1; i<=str.length(); i=i+1) {
+            if (str.charAt(i)>='0' && str.charAt(i)<='9') {
+                
+            }
+            else {
+                cnt = cnt + 1;
+            }
+
+            if (cnt > 0) {
+                return false;
+            }
+
+            if (str == null || str.length() == 0) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        return true;
     }
 }
